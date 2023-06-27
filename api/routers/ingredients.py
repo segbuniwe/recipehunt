fd3c9549dac8a75b821c4e170105b69dc3b721d2
Javier Hernandez <javierohern@me.com>
2023-06-27 16:27:47 -0700

    Created POST route to new ingredient databse with current logged in user

--- api/routers/ingredients.py
+++ api/routers/ingredients.py
@@ -1,16 +1,17 @@
 from fastapi import APIRouter, Depends
 from queries.ingredients import IngredientsRepo, IngredientsIn, IngredientsOut
-
+from authenticator import authenticator
 
 router = APIRouter()
 
 
-@router.post("/api/accounts/{account_id}/ingredients", response_model=IngredientsOut)
+@router.post("/api/ingredients", response_model=IngredientsOut)
 def create_ingredient(
     ingredient: IngredientsIn,
-    account_id: str,
+    account_data: dict = Depends(authenticator.get_current_account_data),
     repo: IngredientsRepo = Depends()
 ):
+    account_id = account_data["id"]
     return repo.create(account_id, ingredient.dict())
 
 # @router.get("/api/accounts/{account_id}/ingredients", response_model=List[RecipesOut])
