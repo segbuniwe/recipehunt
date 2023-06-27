6e14e0145d35d7f678cc92092e7c39f64947c7f4
Shashwath <shash.nayak93@gmail.com>
2023-06-27 11:43:09 -0500

    Adjusted Authentication Queries

--- api/queries/accounts.py
+++ api/queries/accounts.py
@@ -3,9 +3,9 @@ from queries.client import Queries
 from pymongo import MongoClient
 
 
-client = MongoClient("mongodb://admin:password@mongo:27017")
-db = client["recipehunt"]
-collection = db["accounts"]
+# client = MongoClient("mongodb://admin:password@mongo:27017")
+# db = client["recipehunt"]
+# collection = db["accounts"]
 
 
 class DuplicateAccountError(Exception):
@@ -29,16 +29,17 @@ class AccountOutWithPassword(AccountOut):
 
 
 class AccountRepo(Queries):
+    DB_NAME = "recipehunt"
+    COLLECTION = "accounts"
+
     def get(self, email: str) -> AccountOutWithPassword:
-        account = collection.find_one({"email": email})
+        account = self.collection.find_one({"email": email})
         account["id"] = str(account["_id"])
-        del account["_id"]
         return AccountOutWithPassword(**account)
 
     def create(self, info: AccountIn, hashed_password: str) -> AccountOut:
         account = info.dict()
         account["hashed_password"] = hashed_password
-        del account["password"]
-        response = collection.insert_one(account)
+        response = self.collection.insert_one(account)
         account["id"] = str(response.inserted_id)
         return AccountOutWithPassword(**account)
