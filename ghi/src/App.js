010d4b1ebe44a3f1e1daf18fc76be19182354160
Shashwath <shash.nayak93@gmail.com>
2023-07-11 13:02:51 -0400

    Updated changes

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -3,17 +3,28 @@ import Construct from "./Construct.js";
 import ErrorNotification from "./ErrorNotification";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "@galvanize-inc/jwtdown-for-react";
+import SignUpForm from "./SignUpForm.js";
+import LoginForm from "./LoginForm.js";
+
+
 
 function App() {
     return (
         <>
+        <div className="container">
             <BrowserRouter>
-                <div>
-                    <Routes></Routes>
-                </div>
+                <AuthProvider>
+                {/* <TitleBar /> */}
+                <Routes>
+                    {/* <Route exact path="/" element={<Main />}></Route> */}
+                    <Route exact path="/signup" element={<SignUpForm />}></Route>
+                    <Route exact path="/login" element={<LoginForm />}></Route>
+                </Routes>
+                </AuthProvider>
             </BrowserRouter>
+        </div>
         </>
     );
 }
-
 export default App;
--- ghi/src/signupform.js
+++ ghi/src/signupform.js
@@ -1,6 +1,88 @@
+import { useState } from "react";
+import useToken from "@galvanize-inc/jwtdown-for-react";
+import { useNavigate } from "react-router-dom";
 
-function signUpForm() {
-    return <p>Sample Text</p>
-}
+const SignupForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [first, setFirst] = useState("");
+  const [last, setLast] = useState("");
+  const { register } = useToken();
+  const navigate = useNavigate();
 
-export default signUpForm
+  const handleRegistration = (e) => {
+    e.preventDefault();
+    const accountData = {
+      email: email,
+      password: password,
+      first_name: first,
+      last_name: last,
+    };
+    register(
+      accountData,
+      `${process.env.REACT_APP_API_HOST}/api/accounts`
+    );
+    console.log(accountData);
+    e.target.reset();
+    navigate("/");
+  };
+
+  return (
+    <div className="card text-bg-light mb-3">
+      <h5 className="card-header">Signup</h5>
+      <div className="card-body">
+        <form onSubmit={(e) => handleRegistration(e)}>
+          <div className="mb-3">
+            <label className="form-label">email</label>
+            <input
+              name="email"
+              type="text"
+              className="form-control"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+          </div>
+          <div className="mb-3">
+            <label className="form-label">password</label>
+            <input
+              name="password"
+              type="password"
+              className="form-control"
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+          </div>
+          <div className="mb-3">
+            <label className="form-label">first</label>
+            <input
+              name="first_name"
+              type="text"
+              className="form-control"
+              onChange={(e) => {
+                setFirst(e.target.value);
+              }}
+            />
+          </div>
+          <div className="mb-3">
+            <label className="form-label">last</label>
+            <input
+              name="last_name"
+              type="text"
+              className="form-control"
+              onChange={(e) => {
+                setLast(e.target.value);
+              }}
+            />
+          </div>
+          <div>
+            <input className="btn btn-primary" type="submit" value="Register" />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SignupForm;
