bb981719de7e13ce7c7720fccf61a2a3976fc125
Sophia Tony-Egbuniwe <ost2105@columbia.edu>
2023-07-11 12:01:19 -0500

    addded starting endpoints for authentication via apiSlice using redux

---
+++ ghi/src/app/apiSlice.js
@@ -0,0 +1,59 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export const recipeHuntApi = createApi({
+    reducerPath: 'recipeHuntApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: process.env.REACT_APP_API_HOST
+    }),
+    endpoints: (builder) => ({
+        getAccount: builder.query({
+            query: () => ({
+                url: `/token`,
+                credentials: 'include',
+            }),
+            transformResponse: (response) => response ? response.account : null,
+            providesTags: ['Account']
+        }),
+        logout: builder.mutation({
+            query: () => ({
+                url: '/token',
+                method: 'DELETE',
+                credentials: 'include'
+            }),
+            invalidatesTags: ['Account']
+        }),
+        signup: builder.mutation({
+            query: (body) => ({
+                url: `${process.env.REACT_APP_API_HOST}/api/accounts`,
+                method: 'POST',
+                body,
+                credentials: 'include',
+                headers: {
+                    'Content-type': 'application/json'
+                }
+            }),
+            invalidatesTags: ['Account']
+        }),
+        login: builder.mutation({
+            query: ({username, password}) => {
+                const body = new FormData();
+                body.append('username', username);
+                body.append('password', password);
+                return {
+                    url: `/token`,
+                    method: 'POST',
+                    body,
+                    credentials: 'include'
+                }
+            },
+            invalidatesTags: ['Account']
+        }),
+    })
+})
+
+export const {
+    useGetAccountQuery,
+    useLogoutMutation,
+    useLoginMutation,
+    useSignupMutation,
+} = recipeHuntApi;
---
+++ ghi/src/app/store.js
@@ -0,0 +1,13 @@
+import { configureStore } from '@reduxjs/toolkit'
+import { setupListeners } from '@reduxjs/toolkit/query'
+
+import { recipeHuntApi } from './apiSlice'
+
+export const store = configureStore({
+    reducer: {
+        [recipeHuntApi.reducerPath]: recipeHuntApi.reducer
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(recipeHuntApi.middleware)
+})
+
+setupListeners(store.dispatch);
