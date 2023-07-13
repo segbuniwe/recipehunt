67fe523def3c4131a16cf550d6eec056ad5c77bd
Shashwath <shash.nayak93@gmail.com>
2023-07-13 13:42:30 -0400

    Added Search Feature

--- ghi/src/searchPage.js
+++ ghi/src/searchPage.js
@@ -1,13 +1,67 @@
+import { useState } from "react";
 import { useGetRecipesQuery } from "./app/recipeSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { reset, filter } from "./app/searchSlice";
 
 function SearchPage() {
+  const[search, setSearch] = useState('');
+  const searchCriteria = useSelector((state) => state.search.value)
   const { data, isLoading } = useGetRecipesQuery();
-  console.log(data);
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('handleSubmit', search);
+        dispatch(filter(search));
+  }
+
+  const filteredRecipes = () => {
+        if (searchCriteria) {
+            return data.filter(recipe => recipe.title.toLowerCase().includes(searchCriteria))
+        } else {
+            return data;
+        }
+      }
+
   if (isLoading) {
     return <p>Loading...</p>;
   }
-  return  data.map((recipe) =>
-  <h1>{recipe.title}</h1>);
+  return(
+    <>
+    <form className="row" onSubmit={handleSubmit}>
+          <div className="col">
+                <input
+                    className="form-control form-control-lg"
+                    type="text"
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                />
+            </div>
+            <div className="col">
+                <button className="btn btn-lg btn-success" type="submit">Search</button>
+                <button
+                    className="btn btn-lg btn-link"
+                    type="button"
+                    onClick={() => {
+                        dispatch(reset())
+                        setSearch('');
+                    }}
+                >
+                    Reset
+                </button>
+          </div>
+      </form>
+    <div className='mt-3'>
+      <h1>
+          Recipe List{' '}
+          <small className='text-body-secondary'>{searchCriteria}</small>
+      </h1>
+      <div className="row mt-3">
+          {filteredRecipes().map(recipe => <p key={recipe.id}>{recipe.title}</p>)}
+      </div>
+    </div>
+    </>
+  )
 }
 
 export default SearchPage;
