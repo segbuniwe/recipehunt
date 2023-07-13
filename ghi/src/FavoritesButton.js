3fdde55d99cd89e09d242d0e74071d8d10137620
Sophia Tony-Egbuniwe <ost2105@columbia.edu>
2023-07-13 16:26:09 -0500

    created favorites button so that if you have an account you can see it and whether you favorite the recipe or not changes what button you see

--- ghi/src/FavoritesButton.js
+++ ghi/src/FavoritesButton.js
@@ -2,25 +2,28 @@ import { useGetFavoritesQuery } from "./app/recipeSlice";
 import { useEffect, useState } from "react";
 import { useFavoritesMutation } from "./app/recipeSlice";
 import { useDeleteFavoritesMutation } from "./app/recipeSlice";
+import { useParams } from "react-router-dom";
 
-const FavoritesButton = (props) => {
+const FavoritesButton = () => {
     const [favorite, setFavorite] = useState(null);
     const { data: favorites } = useGetFavoritesQuery();
     const [addFavorite] = useFavoritesMutation();
+    const [deleteFavorite] = useDeleteFavoritesMutation();
+    const { recipeId } = useParams();
 
     useEffect(() => {
         if (favorites) {
-            setFavorite(favorites.find((favorite) => favorite.id === props.id) || null);
+            setFavorite(favorites.find((favorite) => favorite.recipe_id === +recipeId) || null);
         }
     }, [favorites]);
-    console.log(favorites)
+    console.log(favorite);
     return (
         <>
             {!favorite && (<button className="btn btn-success"
             onClick={() =>
                 addFavorite({
-                    recipe_id: favorites[0].recipe_id,
-                })
+                    recipe_id: recipeId
+            })
                 }
             >
                 Add to Favorites
@@ -28,7 +31,9 @@ const FavoritesButton = (props) => {
             {favorite && (
             <button
             className="btn btn-danger"
-            onClick={useDeleteFavoritesMutation}
+            onClick={() =>
+                deleteFavorite(favorite.id)
+            }
             >Remove from Favorites
             </button>)}
         </>
--- ghi/src/app/recipeSlice.js
+++ ghi/src/app/recipeSlice.js
@@ -1,58 +1,58 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const recipeApi = createApi({
-  reducerPath: "recipeApi",
-  baseQuery: fetchBaseQuery({
-    baseUrl: process.env.REACT_APP_API_HOST,
-  }),
-  endpoints: (builder) => ({
-    getRecipeById: builder.query({
-      query: (recipeId) => ({
-        url: `/api/recipes/${recipeId}`,
-        credentials: "include",
-      }),
+    reducerPath: "recipeApi",
+    baseQuery: fetchBaseQuery({
+        baseUrl: process.env.REACT_APP_API_HOST,
     }),
-    getRecipes: builder.query({
-      query: () => ({
-        url: "/api/recipes/",
-        credentials: "include",
-      }),
+    endpoints: (builder) => ({
+        getRecipeById: builder.query({
+            query: (recipeId) => ({
+                url: `/api/recipes/${recipeId}`,
+                credentials: "include",
+            }),
+        }),
+        getRecipes: builder.query({
+            query: () => ({
+                url: "/api/recipes/",
+                credentials: "include",
+            }),
+        }),
+        getFavorites: builder.query({
+            query: () => ({
+                url: "/api/favorites/mine",
+                credentials: "include",
+            }),
+            transformResponse: (response) => response.favorites,
+            providesTags: ["Favorites"],
+        }),
+        favorites: builder.mutation({
+            query: (body) => ({
+                url: `/api/favorites`,
+                method: "POST",
+                body,
+                credentials: "include",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            }),
+            invalidatesTags: ["Favorites"],
+        }),
+        deleteFavorites: builder.mutation({
+            query: (favorite_id) => ({
+                url: `/api/favorites/${favorite_id}`,
+                method: "DELETE",
+                credentials: "include",
+            }),
+            invalidatesTags: ["Favorites"],
+        }),
     }),
-    getFavorites: builder.query({
-      query: () => ({
-        url: "/api/favorites/mine",
-        credentials: "include",
-      }),
-      transformResponse: (response) => response.favorites,
-      providesTags: ["Favorites"],
-    }),
-    favorites: builder.mutation({
-      query: (body) => ({
-        url: `/api/favorites/`,
-        method: "POST",
-        body,
-        credentials: "include",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }),
-      invalidatesTags: ["Favorites"],
-    }),
-    deleteFavorites: builder.mutation({
-      query: (favorite_id) => ({
-        url: `/api/favorites/${favorite_id}`,
-        method: "DELETE",
-        credentials: "include",
-      }),
-      invalidatesTags: ["Favorites"],
-    }),
-  }),
 });
 
 export const {
-  useGetRecipeByIdQuery,
-  useGetRecipesQuery,
-  useGetFavoritesQuery,
-  useFavoritesMutation,
-  useDeleteFavoritesMutation,
+    useGetRecipeByIdQuery,
+    useGetRecipesQuery,
+    useGetFavoritesQuery,
+    useFavoritesMutation,
+    useDeleteFavoritesMutation,
 } = recipeApi;
