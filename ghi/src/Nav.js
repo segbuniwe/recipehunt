1032caac84fcbce5b80f732ed45a608a2e2fe86b
Shashwath <shash.nayak93@gmail.com>
2023-07-17 17:45:00 -0400

    Started the Profile Page

--- ghi/src/Nav.js
+++ ghi/src/Nav.js
@@ -27,6 +27,9 @@ function Nav() {
                     {account && <li>
                         <NavLink className="nav-link" aria-current="page" to='/search'>Search Recipes</NavLink>
                     </li>}
+                    {account && <li>
+                        <NavLink className="nav-link" aria-current="page" to='/profile/mine'>My Account</NavLink>
+                    </li>}
                     {account && <button className="btn btn-danger" onClick={logout}>Logout</button>}
                 </ul>
                 </div>
