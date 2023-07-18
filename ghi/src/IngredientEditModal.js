b67ee71e876de76f2329efeaa00760435be89ec9
Javier <javierohern@me.com>
2023-07-18 14:39:52 -0700

    Edit ingredient modal

---
+++ ghi/src/IngredientEditModal.js
@@ -0,0 +1,106 @@
+import { useState } from "react";
+import { useUpdateIngredientMutation } from "./app/apiSlice";
+
+function IngredientEditModal({ ingredient }) {
+  const [name, setName] = useState(ingredient.name);
+  const [amount, setAmount] = useState(ingredient.amount);
+  const [unit, setUnit] = useState(ingredient.unit);
+  const [updateIngredient] = useUpdateIngredientMutation();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const body = {
+      name: name,
+      amount: amount,
+      unit: unit,
+    };
+
+    updateIngredient({
+      body,
+      ingredient_id: ingredient.id,
+    });
+    alert("Ingredient updated successfully.");
+  };
+
+  return (
+    <div
+      className="modal fade"
+      id={`editIngredientModal${ingredient.id}`}
+      tabIndex="-1"
+      aria-labelledby={`editIngredientModalLabel${ingredient.id}`}
+      aria-hidden="true"
+    >
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5
+              className="modal-title"
+              id={`editIngredientModalLabel${ingredient.id}`}
+            >
+              Edit Ingredient
+            </h5>
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div className="modal-body">
+            <form onSubmit={handleSubmit}>
+              <div className="form-floating mb-3">
+                <input
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  placeholder="Name"
+                  required
+                  type="text"
+                  id={`editIngredientName${ingredient.id}`}
+                  className="form-control"
+                  name="name"
+                />
+                <label htmlFor={`editIngredientName${ingredient.id}`}>
+                  Name
+                </label>
+              </div>
+              <div className="form-floating mb-3">
+                <input
+                  value={amount}
+                  onChange={(e) => setAmount(e.target.value)}
+                  placeholder="Amount"
+                  required
+                  type="number"
+                  id={`editIngredientAmount${ingredient.id}`}
+                  className="form-control"
+                  name="amount"
+                />
+                <label htmlFor={`editIngredientAmount${ingredient.id}`}>
+                  Amount
+                </label>
+              </div>
+              <div className="form-floating mb-3">
+                <input
+                  value={unit}
+                  onChange={(e) => setUnit(e.target.value)}
+                  placeholder="Unit"
+                  type="text"
+                  id={`editIngredientUnit${ingredient.id}`}
+                  className="form-control"
+                  name="unit"
+                />
+                <label htmlFor={`editIngredientUnit${ingredient.id}`}>
+                  Unit
+                </label>
+              </div>
+              <button className="btn btn-primary" type="submit">
+                Update
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default IngredientEditModal;
--- ghi/src/ProfilePage.js
+++ ghi/src/ProfilePage.js
@@ -7,6 +7,7 @@ import {
     useDeleteIngredientMutation,
     useUpdateIngredientMutation,
 } from "./app/apiSlice";
+import IngredientEditModal from "./IngredientEditModal";
 
 function ProfilePage() {
     const { data: favorites, isLoading } = useGetFavoritesQuery();
@@ -14,171 +15,167 @@ function ProfilePage() {
     const { data: ingredients, isLoading: ingredientsLoading } =
         useGetIngredientByAccountQuery();
     const [deleteIngredient] = useDeleteIngredientMutation();
+    const [updateIngredient] = useUpdateIngredientMutation();
 
     if (isLoading || ingredientsLoading) {
         return <p>Loading...</p>;
     }
     return (
-        <>
-            <h1>Favorites</h1>
-            {account && (
-                <div
-                    id="carouselExampleControls"
-                    className="carousel slide"
-                    data-bs-ride="carousel"
-                >
-                    <div className="carousel-inner">
-                        <div className="carousel-item active">
-                            <img
-                                src="https://cdn.thememylogin.com/uploads/edd/2019/03/favorites.png"
-                                className="d-block w-100"
-                                alt="Favorites"
-                            />
-                        </div>
-                        {favorites.map((favorite) => {
-                            return (
-                                <div
-                                    className="carousel-item"
-                                    key={favorite.id}
-                                >
-                                    <div className="card mb-3 shadow">
-                                        <img
-                                            src={favorite.thumbnail_url}
-                                            className="card-img-top"
-                                            alt={favorite.name}
-                                        />
-                                        <div className="card-body">
-                                            <h5 className="card-title text-center">
-                                                {favorite.name}
-                                            </h5>
-                                        </div>
-                                        <Link
-                                            className="btn btn-info"
-                                            to={`/recipe/${favorite.recipe_id}`}
-                                        >
-                                            Go to Recipe
-                                        </Link>
-                                    </div>
-                                </div>
-                            );
-                        })}
+      <>
+        <h1>Favorites</h1>
+        {account && (
+          <div
+            id="carouselExampleControls"
+            className="carousel slide"
+            data-bs-ride="carousel"
+          >
+            <div className="carousel-inner">
+              <div className="carousel-item active">
+                <img
+                  src="https://cdn.thememylogin.com/uploads/edd/2019/03/favorites.png"
+                  className="d-block w-100"
+                  alt="Favorites"
+                />
+              </div>
+              {favorites.map((favorite) => {
+                return (
+                  <div className="carousel-item" key={favorite.id}>
+                    <div className="card mb-3 shadow">
+                      <img
+                        src={favorite.thumbnail_url}
+                        className="card-img-top"
+                        alt={favorite.name}
+                      />
+                      <div className="card-body">
+                        <h5 className="card-title text-center">
+                          {favorite.name}
+                        </h5>
+                      </div>
+                      <Link
+                        className="btn btn-info"
+                        to={`/recipe/${favorite.recipe_id}`}
+                      >
+                        Go to Recipe
+                      </Link>
                     </div>
-                    <button
-                        className="carousel-control-prev"
-                        type="button"
-                        data-bs-target="#carouselExampleControls"
-                        data-bs-slide="prev"
-                    >
-                        <span
-                            className="carousel-control-prev-icon"
-                            aria-hidden="true"
-                        ></span>
-                        <span className="visually-hidden">Previous</span>
-                    </button>
-                    <button
-                        className="carousel-control-next"
-                        type="button"
-                        data-bs-target="#carouselExampleControls"
-                        data-bs-slide="next"
-                    >
-                        <span
-                            className="carousel-control-next-icon"
-                            aria-hidden="true"
-                        ></span>
-                        <span className="visually-hidden">Next</span>
-                    </button>
-                </div>
-            )}
-
-            <h1>My Ingredients</h1>
-            {ingredients.length >= 1 ? (
-                <table className="table table-striped">
-                    <thead>
-                        <th>Name</th>
-                        <th>Amount</th>
-                        <th>Unit</th>
-                        <th>Remove</th>
-                        <th>Edit</th>
-                    </thead>
-                    <tbody>
-                        {ingredients.map((ingredient) => {
-                            return (
-                                <tr key={ingredient.id}>
-                                    <td>{ingredient.name}</td>
-                                    <td>{ingredient.amount}</td>
-                                    <td>{ingredient.unit}</td>
-                                    <td>
-                                        <button
-                                            className="btn btn-danger"
-                                            onClick={() =>
-                                                deleteIngredient(ingredient.id)
-                                            }
-                                        >
-                                            Remove Ingredient
-                                        </button>
-                                    </td>
-                                    <td>
-                                        <button
-                                            className="btn btn-danger"
-                                            onClick={() =>
-                                                updateIngredient(ingredient.id)
-                                            }
-                                        >
-                                            Edit Ingredient
-                                        </button>
-                                    </td>
-                                </tr>
-                            );
-                        })}
-                    </tbody>
-                </table>
-            ) : (
-                <p>No ingredients added yet</p>
-            )}
-            {account ? (
-                <div className="text-center mt-3">
-                    <Link
-                        className="btn btn-primary"
-                        data-bs-toggle="modal"
-                        data-bs-target="#exampleModal"
-                        to={`/ingredients`}
-                    >
-                        Add an Ingredient
-                    </Link>
-                </div>
-            ) : (
-                <Link className="btn btn-primary" to={"/login"}>
-                    Login to add an ingredient
-                </Link>
-            )}
-
-            <div
-                className="modal fade"
-                id="exampleModal"
-                tabIndex="-1"
-                aria-labelledby="exampleModalLabel"
-                aria-hidden="true"
-            >
-                <div className="modal-dialog">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h5 className="modal-title" id="exampleModalLabel">
-                                Add Ingredient
-                            </h5>
-                            <button
-                                type="button"
-                                className="btn-close"
-                                data-bs-dismiss="modal"
-                                aria-label="Close"
-                            ></button>
-                        </div>
-                        <div className="modal-body">
-                            <IngredientForm />
-                        </div>
-                    </div>
-                </div>
+                  </div>
+                );
+              })}
             </div>
-        </>
+            <button
+              className="carousel-control-prev"
+              type="button"
+              data-bs-target="#carouselExampleControls"
+              data-bs-slide="prev"
+            >
+              <span
+                className="carousel-control-prev-icon"
+                aria-hidden="true"
+              ></span>
+              <span className="visually-hidden">Previous</span>
+            </button>
+            <button
+              className="carousel-control-next"
+              type="button"
+              data-bs-target="#carouselExampleControls"
+              data-bs-slide="next"
+            >
+              <span
+                className="carousel-control-next-icon"
+                aria-hidden="true"
+              ></span>
+              <span className="visually-hidden">Next</span>
+            </button>
+          </div>
+        )}
+
+        <h1>My Ingredients</h1>
+        {ingredients.length >= 1 ? (
+          <table className="table table-striped">
+            <thead>
+              <th>Name</th>
+              <th>Amount</th>
+              <th>Unit</th>
+              <th>Remove</th>
+              <th>Edit</th>
+            </thead>
+            <tbody>
+              {ingredients.map((ingredient) => {
+                return (
+                  <tr key={ingredient.id}>
+                    <td>{ingredient.name}</td>
+                    <td>{ingredient.amount}</td>
+                    <td>{ingredient.unit}</td>
+                    <td>
+                      <button
+                        className="btn btn-danger"
+                        onClick={() => deleteIngredient(ingredient.id)}
+                      >
+                        Remove Ingredient
+                      </button>
+                    </td>
+                    <td>
+                      <button
+                        className="btn btn-danger"
+                        data-bs-toggle="modal"
+                        data-bs-target={`#editIngredientModal${ingredient.id}`}
+                      >
+                        Edit Ingredient
+                      </button>
+                      <IngredientEditModal ingredient={ingredient} />
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        ) : (
+          <p>No ingredients added yet</p>
+        )}
+        {account ? (
+          <div className="text-center mt-3">
+            <Link
+              className="btn btn-primary"
+              data-bs-toggle="modal"
+              data-bs-target="#exampleModal"
+              to={`/ingredients`}
+            >
+              Add an Ingredient
+            </Link>
+          </div>
+        ) : (
+          <Link className="btn btn-primary" to={"/login"}>
+            Login to add an ingredient
+          </Link>
+        )}
+
+        <div
+          className="modal fade"
+          id="exampleModal"
+          tabIndex="-1"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLabel">
+                  Add Ingredient
+                </h5>
+                <button
+                  type="button"
+                  className="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div className="modal-body">
+                <IngredientForm />
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
     );
 }
 
