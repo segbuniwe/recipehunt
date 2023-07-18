c5c820ce95f7a97e903bedf82516054d2c74151d
Sophia Tony-Egbuniwe <ost2105@columbia.edu>
2023-07-18 10:12:02 -0500

    add carousel of favorites; got carousel to function

--- ghi/src/ProfilePage.js
+++ ghi/src/ProfilePage.js
@@ -1,52 +1,66 @@
-import { useGetFavoritesQuery, useGetRecipeByIdQuery } from "./app/recipeSlice";
+import { useGetFavoritesQuery } from "./app/recipeSlice";
 import { useGetAccountQuery } from "./app/apiSlice";
-import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 function ProfilePage() {
   const { data: favorites, isLoading } = useGetFavoritesQuery();
   const { data: account } = useGetAccountQuery();
+  console.log(favorites);
 
   if (isLoading) {
     return <p>Loading...</p>;
   }
   return (
-    <div
-      id="carouselExampleControls"
-      className="carousel slide"
-      data-bs-ride="carousel"
-    >
-      <div className="carousel-inner">
-        {favorites.map((favorite) => {
-          return (
-            <div className="carousel-item active">
-              <img
-                src={favorite.thumbnail_url}
-                className="d-block w-100"
-                alt={favorite.name}
-              />
-            </div>
-          );
-        })}
-      </div>
-      <button
-        class="carousel-control-prev"
-        type="button"
-        data-bs-target="#carouselExampleControls"
-        data-bs-slide="prev"
+    <>
+      <h1>Favorites</h1>
+      {account && <div
+        id="carouselExampleControls"
+        className="carousel slide"
+        data-bs-ride="carousel"
       >
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-      </button>
-      <button
-        class="carousel-control-next"
-        type="button"
-        data-bs-target="#carouselExampleControls"
-        data-bs-slide="next"
-      >
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-      </button>
-    </div>
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img src="https://cdn.thememylogin.com/uploads/edd/2019/03/favorites.png" className="d-block w-100" alt="Favorites" />
+          </div>
+          {favorites.map((favorite) => {
+            return (
+              <div className="carousel-item">
+                {/* <img
+                  src={favorite.thumbnail_url}
+                  className="d-block w-100"
+                  alt={favorite.name}
+                /> */}
+                <div key={favorite.id} className="card mb-3 shadow">
+                  <img src={favorite.thumbnail_url} className="card-img-top" alt={favorite.name}/>
+                  <div className="card-body">
+                    <h5 className="card-title text-center">{favorite.name}</h5>
+                  </div>
+                  <Link className="btn btn-info" to={`/recipe/${favorite.recipe_id}`}>Go to Recipe</Link>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleControls"
+          data-bs-slide="prev"
+        >
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleControls"
+          data-bs-slide="next"
+        >
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>}
+    </>
   );
 }
 
