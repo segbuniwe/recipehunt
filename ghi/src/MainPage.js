bdb7914aeadd4ce3e3f3c16e48d5646793f01c82
Javier <javierohern@me.com>
2023-07-26 07:20:09 -0700

    Journals, mainpage video replayRate.

--- ghi/src/MainPage.js
+++ ghi/src/MainPage.js
@@ -13,6 +13,7 @@ function MainPage() {
           objectFit: "cover",
           transform: "translate(-50%, -50%)",
           zIndex: "-1",
+          playbackRate: "0.5",
         }}
         autoPlay
         loop
@@ -20,35 +21,8 @@ function MainPage() {
         src={videoSource}
         type="video/mp4"
       />
-            <div className="px-4 py-5 my-5 mt-0 text-center bg-info">
-                <img
-                    className="bg-white rounded shadow d-block mx-auto mb-4"
-                    src="/logo.svg"
-                    alt=""
-                    width="600"
-                />
-                <h1 className="display-5 fw-bold">RecipeHunt</h1>
-                <div className="col-lg-6 mx-auto">
-                    <p className="lead mb-4">
-                    Welcome to RecipeHunt, your ultimate destination for culinary
-                    inspiration! Sign up or log in to access a vast collection of
-                    mouthwatering recipes. Customize your cooking experience by
-                    creating your ingredient list on your profile page, ensuring you never
-                    miss a key ingredient. Discover recipes that match your pantry with
-                    our advanced search feature. Save your favorite finds for easy access,
-                    and embark on a culinary journey filled with delicious flavors.
-                    Happy hunting with RecipeHunt!
-                    </p>
-                    <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-                        <Link
-                            to="/signup"
-                            className="btn btn-primary btn-lg px-4 gap-3"
-                        >
-                            Sign Up!
-                        </Link>
-                    </div>
-                </div>
-            </div>
+
+
         </>
     );
 }
