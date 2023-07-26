a573bd6c75b5c8861d246c584fd6935a6209cba2
Javier <javierohern@me.com>
2023-07-26 12:28:53 -0700

    More mainpage styling

--- ghi/src/MainPage.js
+++ ghi/src/MainPage.js
@@ -1,30 +1,50 @@
-import { Link } from "react-router-dom";
 import videoSource from "./food.mp4";
 function MainPage() {
-    return (
-        <>
-              <video
+  return (
+    <>
+      <div className="container">
+        <video
+          style={{
+            position: "absolute",
+            width: "100%",
+            left: "50%",
+            top: "50%",
+            height: "100%",
+            objectFit: "cover",
+            transform: "translate(-50%, -50%)",
+            zIndex: "-1",
+            playbackRate: "0.5",
+          }}
+          autoPlay
+          loop
+          muted
+          src={videoSource}
+          type="video/mp4"
+        />
+      </div>
+      {/* <div
         style={{
-          position: "absolute",
-          width: "100%",
-          left: "50%",
-          top: "50%",
-          height: "100%",
-          objectFit: "cover",
-          transform: "translate(-50%, -50%)",
-          zIndex: "-1",
-          playbackRate: "0.5",
+          position: "relative",
+          minHeight: "100vh",
+          paddingTop: "100vh",
         }}
-        autoPlay
-        loop
-        muted
-        src={videoSource}
-        type="video/mp4"
-      />
-
-
-        </>
-    );
+      >
+        <header style={{ position: "relative" }}>
+          <h1 className="text-center">Welcome to RecipeHunt!</h1>
+          <p>
+            Welcome to RecipeHunt, your ultimate destination for culinary
+            inspiration! Sign up or log in to access a vast collection of
+            mouthwatering recipes. Customize your cooking experience by creating
+            your ingredient list on your profile page, ensuring you never miss a
+            key ingredient. Discover recipes that match your pantry with our
+            advanced search feature. Save your favorite finds for easy access,
+            and embark on a culinary journey filled with delicious flavors.
+            Happy hunting with RecipeHunt!
+          </p>
+        </header>
+      </div> */}
+    </>
+  );
 }
 
 export default MainPage;
